82474b3cf346f7a68a781011a8b50ee04883d12d
German <you@example.com>
2019-03-31 21:52:59 -0300

    guardar-31-03

---
+++ musicdb/src/main/java/com/itinajero/api/services/IAlbumServices.java
@@ -0,0 +1,16 @@
+package com.itinajero.api.services;
+
+import java.util.List;
+
+import com.itinajero.api.Entity.Album;
+
+
+
+
+public interface IAlbumServices {
+	
+	
+	List<Album> buscarTodos();
+	void guardar(Album album);
+	
+}
